40bf01fdb529a84b59637a56841579a882c67f31
Eric Haase <erichaase@gmail.com>
2018-12-30 21:21:45 -0600

    Ensure successful http status response

--- lib/data_web/clients/espn_api_client.ex
+++ lib/data_web/clients/espn_api_client.ex
@@ -2,6 +2,7 @@ defmodule DataWeb.EspnApiClient do
   use Tesla
 
   plug Tesla.Middleware.BaseUrl, "http://site.api.espn.com"
+  # TODO: use atoms as keys: https://hexdocs.pm/jason/Jason.html#decode/2
   plug Tesla.Middleware.JSON
 
   def scoreboard_game_ids do
@@ -22,7 +23,7 @@ defmodule DataWeb.EspnApiClient do
     }
     get("#{path}?#{URI.encode_query(qs_params)}")
   end
-  defp extract_events({:ok, %{body: %{"events" => events}}}), do: events
-  defp reject_inactive(events), do: Enum.reject(events, &(&1["status"]["type"]["state"] == "pre"))
+  defp extract_events({:ok, %{status: 200, body: body}}), do: body["events"]
+  defp reject_inactive(events) when is_list(events), do: Enum.reject(events, &(&1["status"]["type"]["state"] == "pre"))
   defp map_game_id(events), do: Enum.map(events, &(String.to_integer(&1["id"])))
 end
