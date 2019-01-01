3b992ef930d8ba7ea4301bc2943ffadbfbde5a1c
Eric Haase <erichaase@gmail.com>
2018-12-31 22:28:44 -0600

    Added rating to GameStat struct

--- lib/mix/tasks/fantasy.fetch.ex
+++ lib/mix/tasks/fantasy.fetch.ex
@@ -25,8 +25,7 @@ defmodule Mix.Tasks.Fantasy.Fetch do
   defp process_game_stat(stat) do
     stat
     |> build_game_stat
-    |> inspect
-    |> Mix.shell.info
+    |> insert_rating
     # |> upsert_game_stat
   end
 
@@ -40,19 +39,33 @@ defmodule Mix.Tasks.Fantasy.Fetch do
       espn_id: s.id,
       first_name: s.firstName,
       last_name: s.lastName,
-      min: s.minutes,
-      fgm: fgm,
-      fga: fga,
-      ftm: ftm,
-      fta: fta,
-      tpm: tpm,
-      tpa: tpa,
+      min: String.to_integer(s.minutes),
+      fgm: String.to_integer(fgm),
+      fga: String.to_integer(fga),
+      ftm: String.to_integer(ftm),
+      fta: String.to_integer(fta),
+      tpm: String.to_integer(tpm),
+      tpa: String.to_integer(tpa),
       pts: String.to_integer(s.points),
       reb: String.to_integer(s.rebounds),
       ast: String.to_integer(s.assists),
       blk: String.to_integer(s.blocks),
       stl: String.to_integer(s.steals),
-      to: String.to_integer(s.turnovers)
+      to:  String.to_integer(s.turnovers),
     }
+  end
+
+  defp insert_rating(gs) do
+    fgp = if (gs.fga == 0), do: 0, else: (gs.fgm / gs.fga - 0.464) * (gs.fga / 20.1) * 97.6
+    ftp = if (gs.fta == 0), do: 0, else: (gs.ftm / gs.fta - 0.790) * (gs.fta / 11.7) * 78.1
+    tpm = (gs.tpm - 0.9 ) * 3.5
+    pts = (gs.pts - 15.6) * 0.5
+    reb = (gs.reb - 5.6 ) * 0.9
+    ast = (gs.ast - 3.7 ) * 1.1
+    stl = (gs.stl - 1.1 ) * 6.4
+    blk = (gs.blk - 0.6 ) * 4.9
+    to  = (gs.to  - 2.0 ) * -3.4
+    rating = fgp + ftp + tpm + pts + reb + ast + stl + blk + to
+    Map.put(gs, :rating, rating)
   end
 end
