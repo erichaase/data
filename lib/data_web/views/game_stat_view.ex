73b7ec7610c5cf7fa540c3223610783f272151b4
Eric Haase <erichaase@gmail.com>
2019-01-15 19:14:27 -0600

    Added colors for game stat rows

--- lib/data_web/views/game_stat_view.ex
+++ lib/data_web/views/game_stat_view.ex
@@ -1,3 +1,19 @@
 defmodule DataWeb.GameStatView do
   use DataWeb, :view
+
+  def table_row_class(game_stat) do
+    player_ids = System.get_env("PLAYER_IDS")
+    |> String.split(",")
+    |> Enum.map(&String.to_integer/1)
+
+    if Enum.member?(player_ids, game_stat.espn_player_id) do
+      "mine"
+    else
+      if game_stat.rating >= 0 do
+        "positive"
+      else
+        "negative"
+      end
+    end
+  end
 end
