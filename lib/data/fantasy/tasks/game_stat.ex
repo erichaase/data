f39a93fc8abe0abd7afbc057d629c36d6f6b8ae1
Eric Haase <erichaase@gmail.com>
2019-01-28 05:47:42 -0600

    Removed crash for Rollbar test

--- lib/data/fantasy/tasks/game_stat.ex
+++ lib/data/fantasy/tasks/game_stat.ex
@@ -1,10 +1,6 @@
 defmodule Data.Fantasy.Tasks.GameStat do
   def store(gs) do
     IO.puts "GameStat.process: player: #{gs.first_name} #{gs.last_name} #{gs.rating}"
-    if gs.last_name == "Tolliver" do
-      5 / 0
-    else
-      {:ok, _} = Data.Fantasy.upsert_game_stat(gs)
-    end
+    {:ok, _} = Data.Fantasy.upsert_game_stat(gs)
   end
 end
